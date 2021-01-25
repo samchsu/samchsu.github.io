b0093028cdfd11583a05fd690cc65e5fc9341bfd
samchsu <63684737+samchsu@users.noreply.github.com>
2021-01-24 20:59:57 -0800

    Added Particle Effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import React from 'react';
 import { Navbar } from './components/navbar';
+import {ParticleEffect} from './Particles.js';
 
 const App = () => {
   return (
     <div className="App">
         <Navbar/>
-        hello world
+        <ParticleEffect/>
     </div>
   );
 }
